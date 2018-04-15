9485a43835554246570e33a70505aa88de1b5b93
Cameron Slipp <cdslipp@gmail.com>
2018-04-15 19:21:22 -0400

    Made texter method take class to avoid credential sharing

--- texter.py
+++ texter.py
@@ -1,9 +1,15 @@
 from twilio.rest import Client
+import creds
+
+creds = creds.Credentials()
 
 def sendtext(phone,message):
+    account_sid = creds.account_sid
+    auth_token = creds.auth_token
     client = Client(account_sid, auth_token)
 
     client.api.account.messages.create(
         to=phone,
+        from_=creds.twilionum,
         body=message)
     print("Successfully sent SMS to")
